841b7fb6491dd7e25adb6989883c8985020626b3
Esca97 <128316750+Esca97@users.noreply.github.com>
2023-03-19 18:06:49 +0000

    Add files via upload

---
+++ first_numbers.py
@@ -0,0 +1,10 @@
+for value in range(1,5):
+	print(value)
+numbers=list(range(6))
+print(numbers)
+even_numbers=list(range(2,11,2))
+print(even_numbers)
+squares=[]
+for values in range(1,11):
+	squares.append(values**2)
+print(squares)
